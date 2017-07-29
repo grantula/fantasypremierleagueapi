ccf2081c8a514f768fe80168846a9dfed27670e3
grantula <walter.grant.ward@gmail.com>
2017-07-29 14:06:49 -0400

    added a decorator to ensure when we search for a player or team we are only returning one item, else raising an exception. added info_only kwarg to player to allow us to get just the basic info if we want

--- fantasypremierleagueapi/fantasypremierleagueapi.py
+++ fantasypremierleagueapi/fantasypremierleagueapi.py
@@ -6,7 +6,8 @@ Author: Grant W
 """
 
 # Python Imports
-
+from functools import wraps
+from pprint import pprint
 # Third Party Imports
 import requests
 
@@ -17,50 +18,86 @@ DATA_ENDPOINT = 'bootstrap-static'
 PLAYER_ENDPOINT = 'element-summary/{player_id}'
 
 
+def ensure_one_item(f):
+    """This decorator is used to ensure that when we are searching
+    for a specific item, we return only one result. Otherwise, we
+    raise an exception.
+    """
+    @wraps(f)
+    def wrapped(*args, **kwargs):
+        val = f(*args, **kwargs)
+        if len(val) == 0:
+            raise TypeError('No data found with the given parameters')
+        elif len(val) > 1:
+            raise TypeError('Too much data found with the given parameters.')
+        return val[0]
+    return wrapped
+
 def data():
     """Returns all data from the BASE_URL provided above
     """
     url = '{}/{}'.format(BASE_URL, DATA_ENDPOINT)
     return requests.get(url).json()
 
-def player(name=None, id=None):
+def player(name=None, id=None, info_only=False):
     """Based off name or id, we will return statistics for a
-    specific player.
-    name should be a str "LastName,FirstName"
+    specific player. If we only want basic information on the player,
+    pass the info_only kwarg as True which will not return fixtures
+    and other info.
+
+    id - player id
+    name - name should be a str "LastName,FirstName"
+    info_only - bool, True if we only want basics (Name, current stats, etc)
+    """
+    player_info = _player_info(name=name, id=id)
+    if info_only:
+        return player_info
+    url = '{}/{}'.format(BASE_URL, PLAYER_ENDPOINT)
+    ret_val = requests.get(url.format(player_id=player_info['id'])).json()
+    ret_val['information'] = player_info
+    return ret_val
+
+@ensure_one_item
+def team(name=None, id=None):
+    """Returns data on a given team name or team id
     """
     if not name and not id:
-        raise TypeError("You must provide either a player_name "
-                        "or a player_id.")
-    url = '{}/{}'.format(BASE_URL, PLAYER_ENDPOINT)
-    if name:
-        id = _player_id_from_name(name)
-    return requests.get(url.format(player_id=id)).json()
+        raise TypeError("You must provide either a team name "
+                        "or a team id.")
+    data_ = data()
+    # Id is more specific, so try that first
+    teams = []
+    if id:
+        teams = [x for x in data_['teams'] if x['id'] == id]
+    elif name:
+        teams = [x for x in data_['teams']
+                 if x['name'].lower() == name.lower()]
+    return teams
 
-def _player_id_from_name(name):
-    """Takes a name and attempts to return the ID of the player
-    for interactions with the FPL API.
+@ensure_one_item
+def _player_info(name=None, id=None):
+    """This will return the basic information on a player
     Player Name should be a str "LastName,FirstName"
     """
-    try:
-        first_name = name.split(',')[1].strip()
-        second_name = name.split(',')[0].strip()
-    except IndexError:
-        raise TypeError('Please enter a name in format '
-                        '"LastName,FirstName"')
-    _data = data()
-    player_ids= [x['id'] for x in _data['elements']
-                 if x['first_name'] == first_name
-                 and x['second_name'] == second_name]
-    if len(player_ids) == 0:
-        raise TypeError('No player found with the given name.')
-    elif len(player_ids) > 1:
-        raise TypeError('Multiple players found with the given name.')
-    return player_ids[0]
-
-def team():
-    """
-    """
-    return {}
+    if not name and not id:
+        raise TypeError("You must provide either a player's name "
+                        "or a player's id.")
+    data_ = data()
+    players = []
+    if id:
+        players = [x for x in data_['elements']
+                   if x['id'] == id]
+    elif name:
+        try:
+            first_name = name.split(',')[1].strip()
+            second_name = name.split(',')[0].strip()
+        except IndexError:
+            raise TypeError('Please enter a name in format '
+                            '"LastName,FirstName"')
+        players = [x for x in data_['elements']
+                   if x['first_name'] == first_name
+                   and x['second_name'] == second_name]
+    return players
 
 def game():
     """
